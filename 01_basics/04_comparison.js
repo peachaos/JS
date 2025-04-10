15c4032ec2477ad9f4acbcc8dc8b7a7297136c22
peachaos <amanarya85484@gmail.com>
2025-04-10 15:17:58 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// these type of conversions should be avoided
+console.log("2" > 1)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) //conversion problem
+console.log(null < 0)
+
+//===
+
+console.log("2" === 2)
